69c6fd4214b1ad9fb7b5d5da5a192c899c84f043
Danilo <danilofrancisquete@hotmail.com>
2024-04-17 14:51:47 -0300

    Modal Interaction

--- src/app/shared/modal-pedido/modal-pedido.component.ts
+++ src/app/shared/modal-pedido/modal-pedido.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal-pedido',
   standalone: true,
-  imports: [MatDialogTitle, MatDialogContent],
+  imports: [MatDialogTitle, MatDialogContent,CommonModule,],
   templateUrl: './modal-pedido.component.html',
   styleUrl: './modal-pedido.component.scss'
 })
 export class ModalPedidoComponent {
+  
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+    
+  }
+
+  
+  getSelectedProductImg(): string {
+    return this.data.selectedProduct.img;
+  }
 }
--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -11,6 +11,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { CompanyService } from '../../app/services/company.service';
 import { ModalPedidoComponent } from '../../app/shared/modal-pedido/modal-pedido.component';
 import { ProductService } from '../../app/services/product.service';
+import { title } from 'process';
 
 @Component({
   selector: 'app-home',
@@ -31,12 +32,35 @@ export class HomeComponent implements OnInit {
   company: any;
   productsByCategory: any;
 
+  categoriesTitle: { id: number, name: string}[] = [];
+  selectedCategoryIndex: number = -1;
+  
+
+  
+  onCategoria(index: number): void {
+   
+  if (index >= 0 && index < this.categoriesTitle.length) {
+    this.selectedCategoryIndex = index;
+    const selectedCategoryId = this.categoriesTitle[index].id;
+    const selectedCategory = this.productsByCategory.categories.find((category: { id: number }) => category.id === selectedCategoryId);
+    if (selectedCategory) {
+      const indexToRemove = this.productsByCategory.categories.indexOf(selectedCategory);
+      if (indexToRemove !== -1) {
+        this.productsByCategory.categories.splice(indexToRemove, 1);
+      }
+      this.productsByCategory.categories.unshift(selectedCategory);
+    }
+  }
+  }
+
+
   constructor(
     private companyService: CompanyService,
     private productService: ProductService,
     public dialog: MatDialog,
 
   ) {}
+
 
   async ngOnInit() {
     await this.getCompanyInfo();
@@ -48,17 +72,57 @@ export class HomeComponent implements OnInit {
 
   async getProductInfo() {
     this.productsByCategory = await this.productService.getProducts();
-  
+
+    //EXEMPLO COM FOR
+    for (let index = 0; index < this.productsByCategory?.categories.length; index++) {
+      this.categoriesTitle.push({
+        id: this.productsByCategory?.categories[index].id,
+        name: this.productsByCategory?.categories[index].name,
+      });
+    }
+
+
+    // // EXEMPLO COM FOREACH
+    // this.productsByCategory?.categories.forEach(element:{ id:number, name:string } => {
+    //      this.categoriesTitle.push({
+    //     id: element.id,
+    //     name: element.name
+    //   });
+      
+    // });
+    console.log(this.categoriesTitle);
   }
 
-  openDialog() {
-    this.dialog.open(ModalPedidoComponent, {});
-  }
-
+  openDialog(categoryId:number,productId: number,):void {
  
-
+ const selectedCategory = this.productsByCategory.categories.find((category: { id: number }) => category.id === categoryId);
   
+ 
+ if (selectedCategory) {
+ 
+   const selectedProduct = selectedCategory.products.find((product: { id: number }) => product.id === productId);
 
  
+   if (selectedProduct) {
+    
+     const dialogRef = this.dialog.open(ModalPedidoComponent, {
+       data: {
+         selectedProduct: selectedProduct,
+         selectedCategory:{
+          id: selectedCategory.id,
+            title: selectedCategory.title, 
+            description: selectedCategory.description
+         }
+        }
+     });
 
+   
+     dialogRef.afterClosed().subscribe(result => {
+       console.log('O modal foi fechado', result);
+     });
+   }
+ }
+
+
+}
 }
